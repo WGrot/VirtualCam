112692a2d5712fa8bf97bdd83eb0f81052876ee1
WGrot <117313232+WGrot@users.noreply.github.com>
2025-05-11 19:41:33 +0200

    added cube generator

--- cubeGenerator.py
+++ cubeGenerator.py
@@ -1,3 +1,5 @@
+import random
+
 def add_cube_to_scene_file(filename, center, length):
     cx, cy, cz = center
     l = length / 2.0
@@ -28,4 +30,43 @@ def add_cube_to_scene_file(filename, center, length):
             file.write(line)
 
 
-add_cube_to_scene_file("Scene1.txt", center=(-1.5, 0.0, 5.0), length=1.0)
+def add_cube_faces_to_scene_file(filename, center, length):
+    cx, cy, cz = center
+    l = length / 2.0
+
+    
+    vertices = [
+        (cx - l, cy - l, cz - l),  
+        (cx + l, cy - l, cz - l),  
+        (cx + l, cy + l, cz - l),  
+        (cx - l, cy + l, cz - l),  
+        (cx - l, cy - l, cz + l),  
+        (cx + l, cy - l, cz + l),  
+        (cx + l, cy + l, cz + l),  
+        (cx - l, cy + l, cz + l),  
+    ]
+
+    
+    triangles_by_face = [
+        [(0, 2, 1), (0, 3, 2)],  
+        [(4, 5, 6), (4, 6, 7)],  
+        [(0, 1, 5), (0, 5, 4)],  
+        [(2, 3, 7), (2, 7, 6)],  
+        [(0, 4, 7), (0, 7, 3)],  
+        [(1, 2, 6), (1, 6, 5)],  
+    ]
+
+    with open(filename, 'a') as file:
+        file.write('\n# New cube (random colored triangles)\n')
+        for face in triangles_by_face:
+            R = random.randint(0, 255)
+            G = random.randint(0, 255)
+            B = random.randint(0, 255)
+            for v1_idx, v2_idx, v3_idx in face:
+                v1 = vertices[v1_idx]
+                v2 = vertices[v2_idx]
+                v3 = vertices[v3_idx]
+                line = f"{v1[0]} {v1[1]} {v1[2]} {v2[0]} {v2[1]} {v2[2]} {v3[0]} {v3[1]} {v3[2]} {R} {G} {B}\n"
+                file.write(line)
+add_cube_faces_to_scene_file("scene.txt", center=(-3, 0.0, 7.0), length=1.0)
+#add_cube_to_scene_file("scene.txt", center=(-1.5, 0.0, 5.0), length=1.0)
